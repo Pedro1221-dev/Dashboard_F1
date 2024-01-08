37bf96718196a2c9684afa01c51d5da6948f8ea4
nunomeira11 <40220240@esmad.ipp.pt>
2024-01-08 11:29:45 +0000

    page Login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,64 +1,57 @@
 <template>
-  <v-form @submit.prevent="login">
-  <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
+  <div style="display: flex; justify-content: flex-end;">
+    <div style="flex: 1;">
+      <img src="../img/backgrounds/imageLogin.png" alt="Imagem" style="width: 100%; height: 100vh; object-fit: cover;">
+    </div>
+    <v-form @submit.prevent = "login">
+    <div style="flex: 1; max-width: 450px;">
+      <v-card class="mx-auto pa-12 pb-8" elevation="8" rounded="lg" style="height: 100vh;">
+        <div class="text-h4 text-white mb-4">Acede à tua Conta</div>
 
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+        <v-text-field
+          v-model="username"
+          density="compact"
+          placeholder="Username"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
 
-      <v-text-field
-        v-model="username"
-        density="compact"
-        placeholder="Username"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-      ></v-text-field>
+        <v-text-field
+          v-model="password"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
 
-
-      <v-text-field
-        v-model="password"
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="visible = !visible"
-      ></v-text-field>
-
-
-      <v-btn
-        block
-        class="mb-8"
-        color="blue"
-        size="large"
-        variant="tonal"
-        type="submit"
-      >
-        Log In
-      </v-btn>
-
-      <v-card-text class="text-center">
-        <router-link
-          to="/register"
-          class="text-blue text-decoration-none"
-          rel="noopener noreferrer"
+        <v-btn
+          block
+          class="mb-8"
+          color="white"
+          size="large"
+          variant="tonal"
+          type="submit"
         >
-          Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-        </router-link>
-      </v-card-text>
-    </v-card>
-  </div>
+          Aceder
+        </v-btn>
+
+        <v-card-text class="text-center">
+          <a
+            class="text-3white text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Não tens conta? <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
+    </div>
   </v-form>
+  </div>
 </template>
 
 <script>
@@ -69,13 +62,14 @@ export default {
       store: useUserStore(),
       username: "",
       password: "",
+      visible: false // Adicionando o estado para controlar a visibilidade da senha
     };
   },
   methods: {
     login() {
       try {
         this.store.login(this.username, this.password)
-        this.$router.push({ name: "home" });
+        this.$router.push({ name: "dashboard" });
       } catch (error) {
          alert(`Error: ${error.message}`); 
       }
@@ -84,6 +78,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+html,body {
+  overflow: hidden;
+}
+.v-card {
+  background-color: #242833 !important;
+}
 
+.v-btn {
+  background-color: #F83D49 !important;
+}
 </style>
