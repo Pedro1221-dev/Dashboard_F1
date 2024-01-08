12af87bd42b0bda04fb7e4e1f65aa6c6e7319f77
nunomeira11 <40220240@esmad.ipp.pt>
2024-01-08 11:34:00 +0000

    Page register

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,57 +1,75 @@
 <template>
-  <v-form @submit.prevent="register"> 
-    <v-card
-        class="mx-auto"
-        max-width="344"
-        title="User Registration"
-      >
-        <v-container>
-          <v-text-field
-            v-model="first"
-            color="primary"
-            label="First name"
-            variant="underlined"
-          ></v-text-field>
-    
-          <v-text-field
-            v-model="last"
-            color="primary"
-            label="Last name"
-            variant="underlined"
-          ></v-text-field>
-    
-          <v-text-field
-            v-model="username"
-            color="primary"
-            label="Username"
-            variant="underlined"
-          ></v-text-field>
-    
-          <v-text-field
-            v-model="password"
-            color="primary"
-            label="Password"
-            placeholder="Enter your password"
-            variant="underlined"
-          ></v-text-field>
-    
-        </v-container>
-    
-        <v-divider></v-divider>
-    
-        <v-card-actions>
-          <v-spacer></v-spacer>
-    
-          <v-btn color="success" type="submit">
-            Complete Registration
-    
-            <v-icon icon="mdi-chevron-right" end></v-icon>
-          </v-btn>
-        </v-card-actions>
+  <div style="display: flex; justify-content: flex-end;">
+
+    <div style="flex: 1; max-width: 450px;">
+      <v-card class="mx-auto pa-12 pb-8" elevation="8" rounded="lg" style="height: 100vh;">
+        <div class="text-h4 text-white mb-4">Criar uma Conta</div>
+        <v-form @sunmit.prevent = "register">
+        <!-- Campos de texto para o registro -->
+        <v-text-field
+          v-model="firstName"
+          density="compact"
+          placeholder="Primeiro Nome"
+          prepend-inner-icon="mdi-account-outline"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="lastName"
+          density="compact"
+          placeholder="Último Nome"
+          prepend-inner-icon="mdi-account-outline"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="username"
+          density="compact"
+          placeholder="Nome de Utilizador"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+
+        <!-- Botão de registro -->
+        <v-btn
+          block
+          class="mb-8"
+          color="white"
+          size="large"
+          variant="tonal"
+          type="submit"
+        >
+          Registar
+        </v-btn>
+      </v-form>
+        <!-- Link para página de login -->
+        <v-card-text class="text-center">
+          <router-link
+            to="/login"
+            class="text-white text-decoration-none"
+          >
+            Já tens uma conta? <v-icon icon="mdi-chevron-right"></v-icon>
+          </router-link>
+        </v-card-text>
       </v-card>
-  </v-form>
-    
-  </template>
+    </div>
+
+    <div style="flex: 1;">
+      <img src="imageRegister.png" alt="Nova Imagem" style="width: 100%; height: 100vh; object-fit: cover;">
+    </div>
+  </div>
+</template>
 
 <script>
 import { useUserStore } from "@/stores/user";
@@ -79,6 +97,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style>
+html,body {
+  overflow: hidden;
+}
+.v-card {
+  background-color: #242833 !important;
+}
 
+.v-btn {
+  background-color: #F83D49 !important;
+}
 </style>
