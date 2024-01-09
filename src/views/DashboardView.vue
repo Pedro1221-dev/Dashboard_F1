30284fbd45f2db44b7a82f78af7ff28c966737a4
Pedro <40220243@esmad.ipp.pt>
2024-01-09 14:51:03 +0000

    atualizaçoes feitas na pagina inicial da dashboard

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -28,9 +28,24 @@
             </div>
             <div class="parent">
                 <div class="div1" style="border: 1px solid white;">NOTIFICAÇOES </div>
-                <div class="div2" style="border: 1px solid white;"> CALENDARIO </div>
+                <div class="div2" style="border: 1px solid white;">
+                  CALENDARIO
+                  <div class="calendar-grid">
+                    <div class="day" v-for="day in 30" :key="day">{{ day }}</div>
+                  </div>
+                </div>
                 <div class="div3" style="border: 1px solid white;"> NOTICIAS </div>
-                <div class="div4" style="border: 1px solid white;"> CLASSIFICAÇAO </div>
+                <div class="div4" style="border: 1px solid white;">
+                  CLASSIFICAÇAO
+                  <div class="pilot-card" v-for="pilot in topPilots" :key="pilot.id">
+                    <img :src="pilot.photo" alt="Pilot Photo" class="pilot-photo">
+                    <div class="pilot-info">
+                      <p>{{ pilot.name }}</p>
+                      <p>{{ pilot.number }} - {{ pilot.team }}</p>
+                      <p>Points: {{ pilot.points }}</p>
+                    </div>
+                  </div>
+                </div>
                 <div class="div5" style="border: 1px solid white;">
                   <img src="../img/components/fantasy/Piloto Fantasy.png" style="height: 100%;">
                   <img src="../img/components/fantasy/logo fantasy 1.png" style="width: 40%;">
@@ -48,6 +63,7 @@ export default {
     data() {
         return {
             store: useUserStore(),
+
         };
     },
 
@@ -55,6 +71,39 @@ export default {
         user() {
         return this.store.getUser.username;
         },
+        topPilots() {
+      // Replace this with your actual data or logic to fetch the top pilots
+      return [
+        {
+          id: 1,
+          firstName: "Charles",
+          name: "Leclerc",
+          number: 16,
+          team: "ferrari",
+          points: 338,
+          photo: "../img/drivers/leclerc.png",
+        },
+        {
+          id: 2,
+          firstName: "Lewis",
+          name: "Hamilton",
+          number: 44,
+          team: "mercedes",
+          points: 327,
+          photo: "../img/drivers/hamilton.png",
+        },
+        {
+          id: 3,
+          firstName: "Max",
+          name: "Verstappen",
+          number: 33,
+          team: "redbull",
+          points: 325,
+          photo: "../img/drivers/verstappen.png",
+        },
+      ];
+    },
+
     },
 
     components: {
@@ -92,8 +141,19 @@ export default {
 
 .div1 { grid-area: 1 / 5 / 4 / 7;
  }
-.div2 { grid-area: 4 / 5 / 7 / 7; }
-.div3 { grid-area: 4 / 3 / 7 / 5; }
+ .div2 {
+  grid-area: 4 / 5 / 7 / 7;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.div3 { grid-area: 4 / 3 / 7 / 5;
+  background-image: url('../img/backgrounds/News Photo.png');
+  background-size: cover; /* ou 'contain' se você quiser que a imagem se ajuste dentro da div */
+  background-repeat: no-repeat;
+  background-position: center;
+}
 .div4 { grid-area: 4 / 1 / 7 / 3; }
 .div5 {
   grid-area: 3 / 1 / 4 / 5;
@@ -125,5 +185,31 @@ export default {
 .profile-button{
     background-color: #494949 !important;
     border-radius: 20px !important;
+}
+
+.calendar-grid {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+}
+
+.day {
+  border: 1px solid black;
+  padding: 10px;
+  text-align: center;
+}
+
+.pilot-card {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.pilot-photo {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
 }
 </style>
